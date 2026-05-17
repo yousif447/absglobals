20ec4a538e8fdd91d5e9f27d695dd53d076bb176
yousif447 <yousifadel29@gmail.com>
2026-05-17 11:55:35 +0300

    edit the nest config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "admin.absglobals.com",
-  //       pathname: "/storage/**",
-  //     },
-  //     {
-  //       protocol: "https",
-  //       hostname: "admin.srv1642786.hstgr.cloud",
-  //       pathname: "/storage/**",
-  //     },
-  //   ],
-  // },
   images: {
-    domains: ["admin.srv1642786.hstgr.cloud", "admin.absglobals.com", "absglobals.com"],
+    domains: ["admin.srv1642786.hstgr.cloud", "admin.absglobals.com"],
   }
 };
 
